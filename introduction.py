407befbbc7220fe99d03263fc251f99b846a4772
skarki <karkisandhya35@gmail.com>
2020-03-03 09:54:14 +0545

    my first github

---
+++ introduction.py
@@ -0,0 +1,33 @@
+﻿# # # # print("Hello python")
+
+# # # # python data types:
+
+# # # #print("Hello")
+
+# # # name = "Sophia"
+# # # print(name)
+
+# # # print('Enter name: ')
+# # # name = input()
+
+# # x, y = input('any number example 2,5:').split()
+# # print(x)
+# # print(y)
+
+# num1 = int(input('Enter first number:'))
+# num2 = int(input('Enter second number:'))
+# print(num1+num2)
+# print(type(num1))
+
+# x=10
+# y=20
+# z=x
+# print(x is y)
+# print( x is not y)
+# print(x is z)
+
+data = 'hello python'
+print('ram' in data)
+print('hello' in data)
+
+

1edafd4be1a8b6a124431f5a1bb436bfead1173b
skarki <karkisandhya35@gmail.com>
2020-03-17 08:11:21 +0545

    update

---
+++ dir.py
@@ -0,0 +1,53 @@
+# message = 'Hello python'
+# print(dir(message))
+# print(message.title())
+# print(message.upper())
+# print(message.lower())
+
+# username = 'hello admin'
+
+# x = username.replace('admin','ram')
+# print(x)
+
+# message = 'Hello python we are learning python'
+# print(message.find('we'))
+# print(message.count('python'))
+# print(message.lstrip())
+# print(message.center(100))
+# print(message.endswith('python'))
+# print(message.join('>>'))
+# x = message.split(',')
+# print(x)
+
+# data = ['ram','sita',12345]
+# users = ['gita','sophia','madan']
+
+# total = len(users)
+# x = 0
+# while x < total:
+#     print(users[x])
+#     x += 1
+
+# for user in users:
+#     print(user)
+
+# users.sort(reverse = True)
+# print(users)
+
+# users.remove('gita')
+# users.pop(1)
+# print(users)
+# # data.extend(users)
+# data.append(users)
+# print(data)
+# print(dir(data))
+
+
+
+# data.insert(1,'binita')
+# print(data)
+# data[1] = 'hari'
+# data.append('test')
+# print(data)
+
+#study file handling
